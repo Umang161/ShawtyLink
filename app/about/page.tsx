03d1843cd61f758838a2756fcccb5d56965514c6
Umang Agarwal <umangagarwal2022@gmail.com>
2023-11-18 23:11:26 +0530

    Adds Two New Routes

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About(){
+    return(
+        <div className="text-center">
+            <p>Hey From About Page</p>
+        </div>
+    )
+}
---
+++ app/pricing/page.tsx
@@ -0,0 +1,7 @@
+export default function Pricing(){
+    return(
+        <div className="text-center">
+            <p>Hey From Pricing Page</p>
+        </div>
+    )
+}
